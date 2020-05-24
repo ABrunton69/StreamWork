7b05f30b26071db8a43e7e9d356eb9f4eff0d00c
ABrunton69 <amdrummer2015@gmail.com>
2020-05-24 21:20:53 +0100

    First Command And Response

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ All the work in this repository was created by Aram Brunton on stream, do not re
 # 20:50PM BST
 Added the feature for the virtual assistant to record audio sent from the user into the google cloud server and prints out what the user has said.
 
-# 
+#  21:10PM BST
+Added Response feature for the virtual assistant using an mp3 file and added first command asking how are you.
--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import requests
 
 warnings.filterwarnings('ignore')
 
+
+
 def recordAudio():
     r = sr.Recognizer()
 
@@ -29,4 +31,47 @@ def recordAudio():
 
         return data
 
-# recordAudio()
+def assistantResponse(text):
+
+    print(text)
+
+    myobj = gTTS(text= text, lang='en-uk', slow=False)
+    myobj.save('response.mp3')
+    os.system('start response.mp3')
+
+def wakeWord(text):
+    WAKE_WORDS = ['system']
+
+    text = text.lower()
+
+    for phrase in WAKE_WORDS:
+        if phrase in text:
+            return True
+
+    return False
+
+def greeting(text):
+
+    GREETING_INPUTS = ['yo', 'hello', 'whats up']
+
+    GREETING_RESPONSES = ['yo', 'hello', 'whats good']
+
+    for word in text.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES) + '.'
+    
+    return ''
+
+while True:
+
+    text = recordAudio()
+    response = ''
+
+    if(wakeWord(text)) == True:
+
+        response = response + greeting(text)
+
+        if('how are you' in text):
+            response = 'I am great'
+
+    assistantResponse(response)
